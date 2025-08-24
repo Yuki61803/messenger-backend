6c0d917d399e0d0902affb6d4acadcf63f0986a7
Yuki <Yuki61803@gmail.com>
2025-08-24 12:59:08 +0400

    fix: change module configuration

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UserService } from '../user/user.service';
 import { UserModule } from '../user/user.module';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -20,8 +21,8 @@ import { UserModule } from '../user/user.module';
     }),
     UserModule
   ],
-  providers: [AuthService, ConfigService],
+  providers: [AuthService, ConfigService, AuthGuard],
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [AuthService, AuthGuard, ConfigService, JwtModule],
 })
 export class AuthModule {}
